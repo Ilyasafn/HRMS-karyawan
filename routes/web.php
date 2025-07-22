1b33c89d0daf7fde987de9a6377132b86e3ddf05
Ilyasafn <ilyiyas05@gmail.com>
2025-07-23 01:58:08 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\DivisiController;
+use App\Http\Controllers\JabatanController;
+use App\Http\Controllers\KaryawanController;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('welcome');
+})->name('home');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', function () {
+        return Inertia::render('dashboard');
+    })->name('dashboard');
+
+    // Route::resource('divisi', DivisiController::class);
+    // Route::resource('karyawan', KaryawanController::class);
+    // Route::resource('jabatan', JabatanController::class);
+});
+
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
